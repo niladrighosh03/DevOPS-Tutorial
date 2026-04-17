78e1da608cebb397510b942a8c78b09fe4bd71ca
niladrighosh03 <rockbong225@gmail.com>
2026-04-17 16:41:21 +0530

    feat: initialize project with CI pipeline, testing framework, and entry points

---
+++ test.py
@@ -0,0 +1,7 @@
+import time
+
+def main():
+    time.sleep(3)
+    print("waiting for 3 seconds!...")
+    print("Testing complete")
+
---
+++ test_example.py
@@ -0,0 +1,10 @@
+# import time
+
+# def main():
+#     time.sleep(3)
+#     print("waiting for 3 seconds!...")
+#     print("Testing complete")
+
+
+def test_example():
+    assert 1 + 1 == 2
